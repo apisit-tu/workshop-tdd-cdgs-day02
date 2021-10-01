d77177ac9ffb6cc9cac820ba5a044ce6a42dafd8
CDG\005437 <apisit.tu@cdg.co.th>
2021-10-01 14:53:23 +0700

    Working with spring boot testing

--- src/test/java/com/example/day02/employee/EmployeeControllerTest.java
+++ src/test/java/com/example/day02/employee/EmployeeControllerTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
-class EmployeeControllerTest {
-  
+package com.example.day02.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getById() {
+        EmployeeResponse result = restTemplate.getForObject("/employee/1", EmployeeResponse.class);
+        // Assert
+        assertEquals(1, result.getId());
+        assertEquals("apisit", result.getName());
+    }
 }
