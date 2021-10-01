308ba776b8f4c6b965ff90f161cdf399e3078a1f
CDG\005437 <apisit.tu@cdg.co.th>
2021-10-01 14:31:41 +0700

    initial spring boot

---
+++ src/main/java/com/example/day02/Day02Application.java
@@ -0,0 +1,18 @@
+package com.example.day02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Day02Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Day02Application.class, args);
+		System.out.println(context.getBeanDefinitionCount());
+		//for (String beanDefinitionName : context.getBeanDefinitionNames()) {
+		//	System.out.println(beanDefinitionName);
+		//}
+	}
+
+}
